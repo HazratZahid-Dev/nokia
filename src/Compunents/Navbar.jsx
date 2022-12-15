10972c2907671a9a35ca90c1c626515bf12475b9
HazratZahid9454 <zahid454688@gmail.com>
2022-12-15 11:06:23 +0500

    First Commit

---
+++ src/Compunents/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../CompunentsStyle/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+    <div>
+    <h1>NokIa</h1>
+    </div>
+    <div>
+        <ul className='Pages'>
+            <li>Home</li>
+            <li>Resume</li>
+            <li>About</li>
+            <li>skills </li>
+            <li>Projects</li>
+            <li>Blogs</li>
+            <li>Contact</li>
+
+
+
+        </ul>
+    </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
